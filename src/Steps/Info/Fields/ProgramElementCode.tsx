eaf591a7e1f5433cf50a3219c219c8a96ee4fb96
Gregory Forrest <Gregory.Forrest.1@us.af.mil>
2024-10-29 08:40:29 -0400

    Allow selection of any PEC when used as filter

--- src/Steps/Info/Fields/ProgramElementCode.tsx
+++ src/Steps/Info/Fields/ProgramElementCode.tsx
@@ -5,6 +5,7 @@ import { Option } from "@fluentui/react-components";
 import { useWatch } from "react-hook-form";
 import { useContext } from "react";
 import { globalContext } from "stateManagement/GlobalStore";
+import BACCombobox from "components/BaseFormFields/BACCombobox";
 
 interface IProgramElementCodeProps {
   isFilter?: boolean;
@@ -16,22 +17,53 @@ export const ProgramElementCode = (props: IProgramElementCodeProps) => {
   const programName = useWatch<CAFTOPInfo, "ProgramName">({
     name: "ProgramName",
   });
-  const availablePECS =
-    ProgramNames?.data?.find((item) => item.Title === programName)?.PECs ?? [];
+
+  let availablePECS: string[];
+
+  if (!props.isFilter || programName) {
+    // If we are not in Filter mode, or if we are in Filter mode, and they have selected a Program Name
+    //  then limit selection to PECs associated with that Program Name
+    availablePECS =
+      ProgramNames?.data?.find((item) => item.Title === programName)?.PECs ??
+      [];
+  } else {
+    // If we are in Filter mode, and they haven't selected a Program Name, give them all PECs as options, without duplicates
+    availablePECS = ProgramNames?.data?.flatMap((item) => item.PECs) ?? [];
+    availablePECS = [...new Set(availablePECS)].sort();
+  }
+
   return (
-    <BACDropdown<CAFTOPInfo>
-      name="ProgramElementCode"
-      labelText="Program Element Code (PEC)"
-      rules={{ required: !props.isFilter }}
-      fieldProps={{
-        disabled: !props.isFilter && globalState.wizardMaxStep >= 1,
-      }}
-    >
-      {availablePECS.map((item) => (
-        <Option key={item} value={item} text={item}>
-          {item}
-        </Option>
-      ))}
-    </BACDropdown>
+    <>
+      {!props.isFilter || programName ? (
+        <BACDropdown<CAFTOPInfo>
+          name="ProgramElementCode"
+          labelText="Program Element Code (PEC)"
+          rules={{ required: !props.isFilter }}
+          fieldProps={{
+            disabled: !props.isFilter && globalState.wizardMaxStep >= 1,
+          }}
+        >
+          {availablePECS.map((item) => (
+            <Option key={item} value={item} text={item}>
+              {item}
+            </Option>
+          ))}
+        </BACDropdown>
+      ) : (
+        <BACCombobox<CAFTOPInfo>
+          name="ProgramElementCode"
+          labelText="Program Element Code (PEC)"
+          rules={{ required: !props.isFilter }}
+          fieldProps={{
+            disabled: !props.isFilter && globalState.wizardMaxStep >= 1,
+          }}
+          options={
+            availablePECS.map((item) => {
+              return { children: item, value: item };
+            }) ?? []
+          }
+        />
+      )}
+    </>
   );
 };
