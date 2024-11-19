89cd1ef5b3def698687202d8d5ac79527439505c
Gregory Forrest <Gregory.Forrest.1@us.af.mil>
2024-11-19 13:07:14 -0500

    Update Description page to change placeholders and tooltips

--- src/Steps/Description/Fields/Description.tsx
+++ src/Steps/Description/Fields/Description.tsx
@@ -1,15 +1,20 @@
 import BACTextarea from "@components/BaseFormFields/BACTextarea";
 import { CAFTOPDescription } from "@api/CAFTOP/types";
+import { useDefaultDescription } from "@api/DefaultData";
 
 export const Description = () => {
+  const defaultDescription = useDefaultDescription();
+
   return (
     <BACTextarea<CAFTOPDescription>
       name="Description"
       labelText="Description"
+      labelInfo={`example ${defaultDescription}`}
       rules={{ required: true }}
       fieldProps={{
         rows: 6,
         resize: "vertical",
+        placeholder: "For an example, refer to the tooltip",
       }}
     />
   );
--- src/Steps/Description/Fields/SystemMissionDescription.tsx
+++ src/Steps/Description/Fields/SystemMissionDescription.tsx
@@ -1,16 +1,20 @@
 import BACTextarea from "@components/BaseFormFields/BACTextarea";
 import { CAFTOPDescription } from "@api/CAFTOP/types";
 
+const placeHolderAndTooltip =
+  "For examples, please refer to the CAFTOP Handbook";
+
 export const SystemMissionDescription = () => {
   return (
     <BACTextarea<CAFTOPDescription>
       name="SystemMissionDescription"
       labelText="System/Mission Description"
-      labelInfo="For examples, please refer to the CAFTOP Handbook"
+      labelInfo={placeHolderAndTooltip}
       rules={{ required: true }}
       fieldProps={{
         rows: 6,
         resize: "vertical",
+        placeholder: placeHolderAndTooltip,
       }}
     />
   );
